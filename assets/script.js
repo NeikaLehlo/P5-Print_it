3587f4646085fdacd1ea205f16463b43367fc8e9
Chloé Lemonnier <lemoloe@gmail.com>
2023-06-14 17:21:00 +0200

    Add dots class update

--- assets/script.js
+++ assets/script.js
@@ -17,26 +17,39 @@ const slides = [
 	}
 ]
 
-
+/////////////////////////////////
 /********** VARIABLES **********/
+/////////////////////////////////
+
 //init dotSelected number
 let dotSelected = 0;
+//slidesLength = last index of the table
 const slidesLength = slides.length - 1;
+//dots : all class="dots" in the HTML
+const dots = document.querySelector(".dots");
+//(check)
+console.log( dots);
+//arrowLeft : all class="arrow_left"
+const arrowLeft = document.querySelector(".arrow_left");
+//arrowLeft : all class="arrow_Right"
+const arrowRight = document.querySelector(".arrow_right");
 
-
+/////////////////////////////////
 /********** FUNCTIONS **********/
-//Function for create bullet points
+/////////////////////////////////
+
+// Function for create bullet points
+// scans the "slides" table and creates a dot for each entry
 function generatedots(slides) {
-	const dots = document.querySelector(".dots");
-	document.querySelector(".dots").innerHTML = '';
 	for (let i = 0; i < slides.length; i++) {
 		const dotElement = document.createElement("div");
-		i == dotSelected ? dotElement.setAttribute("class", "dot dot_selected") : dotElement.setAttribute("class", "dot");
+		dotElement.setAttribute("class", "dot");
 		dots.appendChild(dotElement);
 	}
 }
 
 //Function for update image and text of the slides
+//Fetches "banner-img" class and <p> in "banner" id and updates them
 function generateSlides() {
 	const baliseImage = document.querySelector(".banner-img");
 	const baliseText = document.querySelector("#banner p");
@@ -44,27 +57,36 @@ function generateSlides() {
 	baliseText.innerHTML= slides[dotSelected].tagLine;
 }
 
+/////////////////////////////
+/**********SCRIPT **********/
+/////////////////////////////
 
-
-//generate dots for the first time.
 generatedots(slides);
+// dot: all class="dot" in a table ( not create before because the dots don't exist)
+const dot = document.querySelectorAll(".dot");
+//check
+console.log(dot);
+//add dot_selected class on the active dot
+dot[dotSelected].setAttribute("class", "dot dot_selected");
+
 
 //Add EventListener on the arrows with slide update
-const arrowLeft = document.querySelector(".arrow_left");
 arrowLeft.addEventListener("click", function (){
+	dot[dotSelected].setAttribute("class", "dot");
 	dotSelected --;
 	dotSelected < 0 ? dotSelected=slidesLength : null;
-	generatedots(slides);
+	dot[dotSelected].setAttribute("class", "dot dot_selected");
 	generateSlides();
 	return console.log("click arrow left " + dotSelected + " " + slidesLength);
 })
 
 
-const arrowRight = document.querySelector(".arrow_right");
+
 arrowRight.addEventListener("click", function (){
+	dot[dotSelected].setAttribute("class", "dot");
 	dotSelected ++;
 	dotSelected > slidesLength ? dotSelected = 0 : null;
-	generatedots(slides);
+	dot[dotSelected].setAttribute("class", "dot dot_selected");
 	generateSlides();
 	return console.log("click arrow right " + dotSelected + " " + slidesLength);
 })
